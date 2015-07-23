4dc63a72b508a5e156e508ac0f9a4e541b7fb87f
cgoteti <cgoteti@amberleaf.net>
2015-07-23 09:48:26 -0500

    sample Codes for create and update codes

---
+++ ConsoleContacts/SampleClient.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using ConsoleContacts.RightNowService;
+
+namespace ConsoleContacts
+{
+    class sampleClient
+    {
+        //RightNowSyncPortClient is the class exposed by the rightNow webservice.
+        private RightNowSyncPortClient _service;
+
+        public sampleClient()
+        {
+            _service = new RightNowSyncPortClient();
+            _service.ClientCredentials.UserName.UserName = "cgoteti";
+            _service.ClientCredentials.UserName.Password = "goEts#2015";
+        }
+
+        public RNObject[] CreateContact(string FirstName, string LastName,string EmailAddress)
+        {           
+                Contact newContact = populateContactInfo(FirstName, LastName,EmailAddress);
+                //Set the application ID in the client info header
+                ClientInfoHeader clientInfoHeader = new ClientInfoHeader();
+                clientInfoHeader.AppID = ".NET Getting Started test in log API";
+                //Set the create processing options, allow external events and rules to execute
+                CreateProcessingOptions createProcessingOptions = new CreateProcessingOptions();
+                createProcessingOptions.SuppressExternalEvents = false;
+                createProcessingOptions.SuppressRules = false;
+                RNObject[] createObjects = new RNObject[] { newContact };
+                GetProcessingOptions options = new GetProcessingOptions();
+                options.FetchAllNames = false;
+                //Invoke the create operation on the server. This will create a contact 
+                RNObject[] createResults = _service.Create(clientInfoHeader, createObjects, createProcessingOptions);         
+                newContact = createResults[0] as Contact;
+                RNObject[] rnObjects = _service.Get(clientInfoHeader, createResults, options);                
+                return rnObjects;      
+         }
+
+        private Contact populateContactInfo(string fname,string lname,string email)
+        {
+            Contact newContact = new Contact();
+            PersonName personName = new PersonName();
+            personName.First = fname;
+            personName.Last = lname;
+            newContact.Name = personName;
+            Email[] emailArray = new Email[1];
+            emailArray[0] = new Email();
+            emailArray[0].action = ActionEnum.add;
+            emailArray[0].actionSpecified = true;
+            emailArray[0].Address = email;
+            NamedID addressType = new NamedID();
+            ID addressTypeID = new ID();
+            addressTypeID.id = 1;
+            addressType.ID = addressTypeID;
+            addressType.ID.idSpecified = true;
+            emailArray[0].AddressType = addressType;
+            emailArray[0].Invalid = false;
+            emailArray[0].InvalidSpecified = true;
+            newContact.Emails = emailArray;
+            return newContact;
+        }
+
+        static void Main(string[] args)
+        {
+            sampleClient client = new sampleClient();
+            Console.WriteLine("Enter FirstName of contact:");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Enter LastName of contact:");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Enter Email Address");
+            string email = Console.ReadLine();
+            
+            try { 
+          
+                 RNObject[] createdContact = client.CreateContact(fname,lname,email);
+                if (createdContact!=null && createdContact.Count()>0)
+                {
+                    foreach (var item in createdContact)
+                    {
+                        Console.WriteLine("Contact created for " + item.LookupName + " with ID " + item.ID.id);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No contact created");
+                }
+                
+            }
+            catch (System.ServiceModel.FaultException ex)
+            {
+                 Console.WriteLine(ex.Code);
+                 Console.WriteLine(ex.Message);
+            }
+
+            System.Console.Read();
+
+        }
+    }
+}
---
+++ QueryCSVsamples/QueryCSV.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using QueryCSVsamples.RightNowServiceReference;
+using System.ServiceModel;
+
+namespace QueryCSVsamples
+{
+    class QueryCSV
+    {
+        private RightNowSyncPortClient _client;
+        public QueryCSV()
+        {
+            _client = new RightNowSyncPortClient();
+            _client.ClientCredentials.UserName.UserName = "cgoteti";
+            _client.ClientCredentials.UserName.Password = "goEts#2015";
+        }
+
+        #region QueryCSV with Where clause        
+        public void QueryCSVwithWhere() {
+            ClientInfoHeader clientInfoHeader = new ClientInfoHeader();
+            clientInfoHeader.AppID = "CSV query on Contacts with where clause";
+
+            // string queryString = "SELECT contact.LookupName from contact where contact.ID>6";
+             string queryString = "SELECT * FROM contact";
+           // string queryString = "SELECT I.CustomFields.CO FROM Incident I WHERE ID = 27;";
+
+            try
+            {
+                byte[] rawResult = null;
+                CSVTableSet queryCSVwithWhere = _client.QueryCSV(clientInfoHeader, queryString, 1000, ",", false, true, out rawResult);
+                CSVTable[] csvTables = queryCSVwithWhere.CSVTables;
+                foreach (CSVTable table in csvTables)
+                {
+                    System.Console.WriteLine("Name: " + table.Name);
+                    System.Console.WriteLine("Columns: " + table.Columns);
+                    String[] rowData = table.Rows;
+
+                    foreach (String data in rowData)
+                    {
+                        System.Console.WriteLine("Row Data: " + data);
+                    }
+                }
+            }
+            catch (System.ServiceModel.FaultException ex)
+            {
+                Console.WriteLine(ex.Code);
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+
+        #region QueryObject
+        public void queryObjects()
+        {
+            ClientInfoHeader clientHeaderInfo = new ClientInfoHeader();
+            clientHeaderInfo.AppID = "Chandu test query objects";
+             string queryString = "SELECT O.Opportunities FROM Organization O WHERE O.ID = 1;";
+           // string queryString = "Select * from contact";
+            
+            Opportunity opportunity = new Opportunity();
+
+            RNObject[] objectTemplates = new RNObject[] { opportunity };
+
+            try
+            {
+                QueryResultData[] queryObjects = _client.QueryObjects(clientHeaderInfo, queryString, objectTemplates, 10000);
+                RNObject[] rnObjects = queryObjects[0].RNObjectsResult;
+
+                foreach (RNObject obj in rnObjects)
+                {                    
+                    Opportunity Opportunity = (Opportunity)obj;                    
+                    Console.WriteLine("Opportunity of the organization is - " + Opportunity.Name);
+                }
+
+            }
+            catch (System.ServiceModel.FaultException ex)
+            {
+                Console.WriteLine(ex.Code);
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+
+        #region update an object    
+        public void updateContact(long id, string updatedfirstname) {
+            Contact contacttobeUpdated = new Contact();
+            ID contactID = new ID();
+            contactID.id = id;
+            contactID.idSpecified = true;
+            contacttobeUpdated.ID = contactID;
+
+            PersonName pn = new PersonName();
+            pn.First = updatedfirstname;
+            
+
+            contacttobeUpdated.Name = pn;
+            RNObject[] rnobjects = new RNObject[]{ contacttobeUpdated };
+            UpdateProcessingOptions options = new UpdateProcessingOptions();
+            options.SuppressExternalEvents = false;
+            options.SuppressRules = false;
+            try
+            {
+                ClientInfoHeader clientInfoHeader = new ClientInfoHeader();
+                clientInfoHeader.AppID = "Basic Update";
+
+                //Invoke the Update operation
+                _client.Update(clientInfoHeader, rnobjects, options);
+            }
+            catch (FaultException ex)
+            {
+                Console.WriteLine(ex.Code);
+                Console.WriteLine(ex.Message);
+            }          
+        } 
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            QueryCSV proxyclient = new QueryCSV();
+            try
+            {
+                proxyclient.QueryCSVwithWhere();
+                // proxyclient.queryObjects();
+                Console.WriteLine();
+                Console.WriteLine("Enter the ID of Contact you want to update: ");
+                long _id = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter first name you want to update for the contact:");
+                string _fname = Console.ReadLine();
+               // long _id1 = Convert.ToInt64(_id);
+                proxyclient.updateContact(_id, _fname);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
